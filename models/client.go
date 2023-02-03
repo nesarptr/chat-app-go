3bb921a4301c6b7980fa23ad527d0968d9398fba
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-02-03 20:16:17 +0600

    create function added client and text models

--- models/client.go
+++ models/client.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
 type Client struct {
 	gorm.Model
-	UserName     string  `json:"username" validate:"required,min=3,max=32" gorm:"unique"`
-	Password string  `json:"password" validate:"required,min=6"`
-	Texts    []Text  `json:"texts" validate:"-"`
+	UserName string `json:"username" validate:"required,min=3,max=32" gorm:"unique"`
+	Password string `json:"password" validate:"required,min=6"`
+	Texts    []Text `json:"texts" validate:"-"`
+}
+
+func (c *Client) Create(db *gorm.DB) error {
+	if db.Create(c).Error != nil {
+		return fiber.ErrBadRequest
+	}
+	return nil
 }
--- models/text.go
+++ models/text.go
@@ -1,12 +1,22 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
 type Text struct {
 	gorm.Model
-	Body string `json:"body" validate:"required,min=1"`
-	SenderID      uint    `json:"sender" validate:"required"`
-	Sender      Client    `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:SenderID;references:ID" validate:"-"`
-	ReceiverID      uint    `json:"receiver" validate:"required"`
-	Receiver      Client    `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ReceiverID;references:ID" validate:"-"`
+	Body       string `json:"body" validate:"required,min=1"`
+	SenderID   uint   `json:"sender" validate:"required"`
+	Sender     Client `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:SenderID;references:ID" validate:"-"`
+	ReceiverID uint   `json:"receiver" validate:"required"`
+	Receiver   Client `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ReceiverID;references:ID" validate:"-"`
+}
+
+func (t *Text) Create(db *gorm.DB) error {
+	if db.Create(t).Error != nil {
+		return fiber.ErrBadRequest
+	}
+	return nil
 }
